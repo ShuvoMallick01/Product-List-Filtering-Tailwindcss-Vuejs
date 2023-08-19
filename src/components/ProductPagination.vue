c68599dcf575f1bab05e799a3b24ad537f6ee8bf
ShuvoMallick01 <shuvacse01@gmail.com>
2023-08-19 16:01:49 +0600

    design Fixing

--- src/components/ProductPagination.vue
+++ src/components/ProductPagination.vue
@@ -7,6 +7,7 @@
           class="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           @click="handlePrev()"
           :class="{ 'cursor-not-allowed': currentPage === 1 }"
+          :disabled="currentPage === 1"
         >
           Prev
         </button>
@@ -30,6 +31,7 @@
           href="#"
           class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           :class="{ 'cursor-not-allowed': currentPage === pageCount }"
+          :disabled="currentPage === pageCount"
           @click="handleNext()"
         >
           Next
